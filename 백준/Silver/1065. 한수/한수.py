29c195f6600c5ad0f0d6d5cc89cfd0746d22a627
leejw0587 <90506657+leejw0587@users.noreply.github.com>
2024-03-21 14:32:13 +0900

    [Silver IV] Title: 한수, Time: 68 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1065. 한수/한수.py
@@ -0,0 +1,13 @@
+def solve(num):
+    count = 0
+    for i in range(1, num+1):
+        num_list = list(map(int, str(i)))
+        if i < 100:
+            count += 1
+        elif num_list[0] - num_list[1] == num_list[1]-num_list[2]:
+            count += 1
+    return count
+
+
+num = int(input())
+print(solve(num))
