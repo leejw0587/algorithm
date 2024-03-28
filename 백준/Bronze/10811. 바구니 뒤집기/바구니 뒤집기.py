7f4e36a663007d1b22385ad3583cb77532429e47
leejw0587 <90506657+leejw0587@users.noreply.github.com>
2024-03-28 13:51:00 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
@@ -0,0 +1,14 @@
+N, M = map(int, input().split())
+
+basket = list(range(1, N+1))
+
+
+for _ in range(M):
+    i, j = map(int, input().split())
+    
+    temp = basket[i-1:j]
+    temp.reverse()
+    basket[i-1:j] = temp
+    
+for i in range(N):
+    print(basket[i], end=' ')
