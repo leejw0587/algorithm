7fcdad50b896736569c1f817c65d2e35321a1ca5
leejw0587 <90506657+leejw0587@users.noreply.github.com>
2024-10-25 00:08:21 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 1124 KB -BaekjoonHub

--- 백준/Bronze/9086. 문자열/문자열.cc
+++ 백준/Bronze/9086. 문자열/문자열.cc
@@ -1,18 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
 
-int main(void){
-    int T = 0;
-    int i = 0;
-    char string[1000] = {0};
+int main(void) {
+	int T = 0;
+	int i = 0;
+	char string[1001];
 
-    scanf("%d", &T);
-    for(i = 0; i < T; i++){
-        scanf(" %s", &string[0]);
-        printf("%c%c\n", string[0], string[strlen(string) - 1]);
+	scanf("%d", &T);
+	
+	for(i = 0; i < T; i++){
+		scanf("%s", string);
+		printf("%c%c\n", string[0], string[strlen(string) - 1]);
+	}
 
-        memset(string, '\0', sizeof(string));
-    }
-
-    return 0;
+	return 0;
 }
